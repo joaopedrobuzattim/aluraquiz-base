4ac4d56471dbc22aa7fec303ce12ec9e9e59200f
Joao Pedro Buzatti Mendes <joao.buzattimendes@gmail.com>
2021-01-26 13:46:17 -0300

    Updating GitHubCorner

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/joaopedrobuzattim" />
     </QuizBackground>
   );
 }
