cf8ddf60d1c9a6e708fa497e83e239f79f935f98
Joao Pedro Buzatti Mendes <joao.buzattimendes@gmail.com>
2021-01-28 13:18:18 -0300

    Aula 4

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,63 @@
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import QuizLogo from '../src/components/QuizLogo'
-
-
- const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Widget from '../src/components/Widget';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    <>
+      <Head>
+        <title> My Alura Quiz </title>
+      </Head>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Task list do Projeto</h1>
-
-            <p>Implementar modificacoes no Quiz Original </p>
-
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/joaopedrobuzattim" />
-    </QuizBackground>
+          <Widget>
+            <Widget.Content>
+              <form onSubmit={function (e) {
+                e.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                // Router manda para a proxima pagina
+              }}
+              >
+                <Input
+                  name="nomeDoUsuario"
+                  onChange={(event)=> setName(event.target.value)}
+                  placeholder="Diz ai seu nome"
+                  value={name}
+                />
+                <Button type="submit" disabled={name.length === 0}>
+                  {`Jogar ${name}`}
+                </Button>
+              </form>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/joaopedrobuzattim" />
+      </QuizBackground>
+    </>
   );
 }
